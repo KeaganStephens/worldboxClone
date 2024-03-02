7910dc755d02203568bedadd999df9da336dead4
Stephens <Keagan.stephens@icloud.com>
2024-03-02 21:05:47 +0200

    project setup

---
+++ src/app/world-canvas/world-canvas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-world-canvas',
+  standalone: true,
+  imports: [],
+  templateUrl: './world-canvas.component.html',
+  styleUrl: './world-canvas.component.css'
+})
+export class WorldCanvasComponent {
+
+}
